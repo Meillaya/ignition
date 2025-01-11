87b4f03668306533a0ff747e63368c656a00cfc3
Meillaya <nathanagbomed@proton.me>
2025-01-11 17:02:33 -0500

    refactor: enforce notNull constraints on name and age fields in users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,11 +2,11 @@ import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }),
-  age: integer(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255}).notNull(),
-  role: varchar({ length: 50}).notNull(),
+  password: varchar({ length: 255 }).notNull(),
+  role: varchar({ length: 50 }).notNull(),
   createdAt: timestamp('created_at').defaultNow(),
   updatedAt: timestamp('created_at').defaultNow(),
 });
